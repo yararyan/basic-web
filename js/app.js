ec59da751df49b6058bca29ee7e478a91d1b14ab
yararyan <yara.ryan@uts.edu.au>
2016-05-11 13:26:30 +1000

    adding in services to call json file

--- js/app.js
+++ js/app.js
@@ -1,20 +1,13 @@
 (function(){
-    var app = angular.module('store', [ ]);
+    var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', function(){
-        this.products = gems;
-    })
-
-    app.controller('PanelController', function(){
-        this.tab = 1;
-
-        this.selectTab = function(setTab){
-            this.tab = setTab;
-        }
-        this.isSelected = function(checkTab){
-            return this.tab === checkTab;
-        }
-    })
+    app.controller('StoreController', ['$http', function($http){
+        var store = this;
+        store.products = [ ];
+        $http.get('/basic-web/products.json').success(function(data){
+            store.products = data;
+        });
+    }]);
 
     app.controller('ReviewController', function(){
         this.review = {};
@@ -24,65 +17,4 @@
             this.review = {};
         }
     })
-
-
-    app.directive('productTitle', function(){
-        return{
-            restrict: 'E',
-            templateUrl: 'product-title.html'
-        }
-    })
-
-
-    var gems = [
-        {
-            name: 'Dodecahedron',
-            price: 2,
-            description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-            canPurchase: true,
-            images: [
-                {
-                    full: 'dodecahedron-01-full.jpg',
-                    thumb: 'dodecahedron-01-thumb.jpg'
-                }
-            ],
-            reviews: [
-                {
-                    stars: 5,
-                    body: "I love this product",
-                    author: "joe@thomas.com"
-                },
-                {
-                    stars: 1,
-                    body: "This product sucks",
-                    author: "tom@hater.com"
-                }
-            ]
-        },
-        {
-            name: 'Pentagonal Gem',
-            price: 5.95,
-            description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Pentagonal is one of those gems.',
-            canPurchase: true,
-            images: [
-                {
-                    full: 'dodecahedron-02-full.jpg',
-                    thumb: 'dodecahedron-02-thumb.jpg'
-                }
-            ],
-            reviews: [
-                {
-                    stars: 5,
-                    body: "I love this product",
-                    author: "joe@thomas.com"
-                },
-                {
-                    stars: 1,
-                    body: "This product sucks",
-                    author: "tom@hater.com"
-                }
-            ]
-
-        }
-    ]
 })();
