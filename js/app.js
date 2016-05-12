5d27b99e53fe689e26825efc9595b3cfb563b1d8
yararyan <yara.ryan@uts.edu.au>
2016-05-12 13:54:20 +1000

    adding data back into app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 (function(){
     var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', ['$http', function($http){
+    app.controller('StoreController', function(){
+        this.products = gems;
+    });
+
+    /*app.controller('StoreController', ['$http', function($http){
         var store = this;
         store.products = [ ];
         $http.get('http://cfsites1.uts.edu.au/gsa/products.html').success(function(data){
             store.products = data;
         });
-    }]);
+    }]);*/
 
     app.controller('ReviewController', function(){
         this.review = {};
@@ -17,4 +21,56 @@
             this.review = {};
         }
     })
+
+    var gems = [
+        {
+            "name": "Dodecahedron",
+            "price": 2,
+            "description": "Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.",
+            "canPurchase": true,
+            "images": [
+                {
+                    "full": "dodecahedron-01-full.jpeg",
+                    "thumb": "dodecahedron-01-thumb.jpeg"
+                }
+            ],
+            "reviews": [
+                {
+                    "stars": 5,
+                    "body": "I love this product",
+                    "author": "joe@thomas.com"
+                },
+                {
+                    "stars": 1,
+                    "body": "This product sucks",
+                    "author": "tom@hater.com"
+                }
+            ]
+        },
+        {
+            "name": "Pentagonal Gem",
+            "price": 5.95,
+            "description": "Some gems have hidden qualities beyond their luster, beyond their shine... Pentagonal is one of those gems.",
+            "canPurchase": true,
+            "images": [
+                {
+                    "full": "dodecahedron-02-full.jpeg",
+                    "thumb": "dodecahedron-02-thumb.jpeg"
+                }
+            ],
+            "reviews": [
+                {
+                    "stars": 5,
+                    "body": "I love this product",
+                    "author": "joe@thomas.com"
+                },
+                {
+                    "stars": 1,
+                    "body": "This product sucks",
+                    "author": "tom@hater.com"
+                }
+            ]
+        }
+    ]
+
 })();
